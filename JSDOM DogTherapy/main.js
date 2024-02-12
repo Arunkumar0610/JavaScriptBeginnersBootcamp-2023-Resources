d359bea6a775f28787a1a36cc37c6d6e1576ff82
Arun kumar <49372861+Arunkumar0610@users.noreply.github.com>
2024-02-13 01:34:42 +0530

    added DogTherapy and frontEnd html,css and cookbook

---
+++ JSDOM DogTherapy/main.js
@@ -0,0 +1,16 @@
+var image = document.querySelector('.dog-image');
+ 
+function fetchDog() {
+  console.log("click registerd")
+  console.log(image);
+  fetch('https://dog.ceo/api/breeds/image/random').then(response => {
+  return response.json();  
+  
+  }).then(jsonResponse => {
+    image.src=jsonResponse.message;
+  })
+}
+
+var fetchDogButton=document.querySelector('button');
+
+fetchDogButton.addEventListener('click',fetchDog);
